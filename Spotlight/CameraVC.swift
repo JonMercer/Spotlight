dda0e50932d4d52814c451121f4bf3aed0d9b99c
JonMercer <mercer.jon@gmail.com>
2016-06-26 13:51:56 -0700

    #1 Added VC

---
+++ Spotlight/CameraVC.swift
@@ -0,0 +1,27 @@
+//
+//  CameraVC.swift
+//  Spotlight
+//
+//  Created by Odin on 2016-06-26.
+//  Copyright © 2016 SpotlightTeam. All rights reserved.
+//
+
+
+import UIKit
+
+class CameraVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("fffF")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
