e31662cf73b2f8eba47fd11a5528fa750e403982
Yosuke Sugishita <yosuke.sugi@gmail.com>
2016-06-30 01:22:28 -0400

    dynamically generating (kind of) cells in a table view!!!  YES.

---
+++ Spotlight/SpotCell.swift
@@ -0,0 +1,24 @@
+//
+//  SpotCell.swift
+//  Spotlight
+//
+//  Created by Yosuke Sugishita on 6/30/16.
+//  Copyright © 2016 SpotlightTeam. All rights reserved.
+//
+
+import UIKit
+
+class SpotCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
