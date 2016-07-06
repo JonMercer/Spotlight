ef12d418408331f68e78296b4b59841f7373cbfd
JonMercer <mercer.jon@gmail.com>
2016-07-05 19:33:10 -0700

    SL-58 Writing and reading from firebase

--- Spotlight/FeedbackVC.swift
+++ Spotlight/FeedbackVC.swift
@@ -10,20 +10,61 @@ import UIKit
 import Firebase
 
 class FeedbackVC: UIViewController {
+  let name = "joe"
   
+  @IBAction func signInButton(sender: AnyObject) {
+    //MARK: Log in
+    FIRAuth.auth()?.signInWithEmail("\(name)@g.com", password: "123456") { (user, error) in
+      if (error != nil) {
+        print("Could not log in creating a user:\(self.name)")
+        
+        //MAKR: Create a user
+        FIRAuth.auth()?.createUserWithEmail("\(self.name)@g.com", password: "123456") { (user, error) in
+          if (error != nil) {
+            print("Could not create a user:\(self.name)")
+          } else {
+            print("Created a user:\(self.name)")
+          }
+        }
+      } else {
+        print("User:\(self.name) logged in")
+      }
+    }
+  }
+  
+  @IBAction func sendAnItemButton(sender: AnyObject) {
+    //MARK: Write to FIR
+    let ref = FIRDatabase.database().reference()
+    if let user = FIRAuth.auth()?.currentUser {
+      // User is signed in.
+      ref.child("users").child(user.uid).setValue(["username": "\(self.name) made this"])
+      print("user:\(self.name) created an entry")
+    } else {
+      // No user is signed in.
+    }
+  }
+  
+  @IBAction func printTextFromFirebase(sender: AnyObject) {
+    //MARK: Read from FIR
+    let ref = FIRDatabase.database().reference()
+    let userID = FIRAuth.auth()?.currentUser?.uid
+    ref.child("users").child(userID!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+      // Get user value
+      let messageFromFirebase = snapshot.value!["username"] as! String
+      //let user = User.init(username: username)
+      print("Printing \(self.name)'s message: \(messageFromFirebase)")
+      
+      // ...
+    }) { (error) in
+      print(error.localizedDescription)
+    }
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
     print("Opened FeedbackVC")
     // Do any additional setup after loading the view, typically from a nib.
+
     
-    //Firebase required authenticated users
-    FIRAuth.auth()?.createUserWithEmail("jane2@g.com", password: "123456") { (user, error) in
-      if (error != nil) {
-        print("\nCould not log in")
-      } else {
-        print("\nUser logged in")
-      }
-    }
   }
   
   override func didReceiveMemoryWarning() {
