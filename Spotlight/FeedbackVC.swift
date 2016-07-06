97d99c5df1cee7f39be2b10b9a805ded72ffbbac
JonMercer <mercer.jon@gmail.com>
2016-07-05 18:38:49 -0700

    SL-58 moved Feedback to its own view controller

---
+++ Spotlight/FeedbackVC.swift
@@ -0,0 +1,26 @@
+//
+//  FeedbackVC.swift
+//  Spotlight
+//
+//  Created by Odin on 2016-07-05.
+//  Copyright © 2016 SpotlightTeam. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("Opened FeedbackVC")
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+}
+
