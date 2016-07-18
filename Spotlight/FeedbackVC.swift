781dbf2f9e7ad28a7bad1f599cd77bbd63bbee5b
Tanha <tanhakabir.ca@gmail.com>
2016-07-17 18:58:48 -0700

    SL-80 map loading

--- Spotlight/FeedbackVC.swift
+++ Spotlight/FeedbackVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import GoogleMaps
 
 class FeedbackVC: UIViewController {
   let name = "jojo"
@@ -20,6 +21,19 @@ class FeedbackVC: UIViewController {
     signIn()
     
     LocationManager.sharedInstance.stopGettingLoc()
+    
+    
+    let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+                                                      longitude: 151.20, zoom: 6)
+    let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+    mapView.myLocationEnabled = true
+    self.view = mapView
+    
+    let marker = GMSMarker()
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+    marker.title = "Sydney"
+    marker.snippet = "Australia"
+    marker.map = mapView
   }
   
   override func didReceiveMemoryWarning() {
