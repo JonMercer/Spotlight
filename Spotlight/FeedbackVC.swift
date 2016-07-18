10860c2425227402a12906da6e5f1a15eca301be
Tanha <tanhakabir.ca@gmail.com>
2016-07-17 21:14:44 -0700

    SL-80 loads map

--- Spotlight/FeedbackVC.swift
+++ Spotlight/FeedbackVC.swift
@@ -22,19 +22,6 @@ class FeedbackVC: UIViewController {
     
     LocationManager.sharedInstance.stopGettingLoc()
     
-    
-    let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-                                                      longitude: 151.20, zoom: 6)
-    let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-    mapView.myLocationEnabled = true
-    self.view = mapView
-    
-    let marker = GMSMarker()
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-    marker.title = "Sydney"
-    marker.snippet = "Australia"
-    marker.map = mapView
-    
   }
   
   override func didReceiveMemoryWarning() {
