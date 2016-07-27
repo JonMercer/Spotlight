564961931ad9ce6201bd1725a7a2b5101a259196
JonMercer <mercer.jon@gmail.com>
2016-07-26 21:26:03 -0700

    SL-143 fied initial location as zero bug

--- Spotlight/LoadingVC.swift
+++ Spotlight/LoadingVC.swift
@@ -13,13 +13,19 @@ class LoadingVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        LocationManager.sharedInstance.startGettingLoc()
+
+        // play ad every 5 minutes
+        NSTimer.scheduledTimerWithTimeInterval(4, target: self, selector: #selector(LoadingVC.goToTabView), userInfo: nil, repeats: true)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
+    }
+    
+    func goToTabView() {
+        performSegueWithIdentifier(Segues.toTabView, sender: self)
     }
 
 }
