f5a903a3b8b398651c36fe6f428e1c1bfce821a2
JonMercer <mercer.jon@gmail.com>
2016-08-16 20:20:50 -0700

    SL-188 added view controllers

---
+++ Spotlight/MeVC.swift
@@ -0,0 +1,22 @@
+//
+//  MeVC.swift
+//  Spotlight
+//
+//  Created by Odin on 2016-08-16.
+//  Copyright © 2016 SpotlightTeam. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
