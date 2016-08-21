d1385476cb86ab55f2767649081091c7997c8d9f
JonMercer <mercer.jon@gmail.com>
2016-08-21 12:19:38 -0700

    SL-185 added View Controller

---
+++ Spotlight/PhotoVC.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoVC.swift
+//  Spotlight
+//
+//  Created by Odin on 2016-08-21.
+//  Copyright © 2016 SpotlightTeam. All rights reserved.
+//
+
+import UIKit
+
+class PhotoVC: UIViewController {
+
+}
