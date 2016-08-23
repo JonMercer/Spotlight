12aaf29e78dc8bdba246f906cb998cb2e7c58a5a
Tanha <tanhakabir.ca@gmail.com>
2016-08-22 18:18:53 -0700

    SL-199 added VCs for login and auth

---
+++ Spotlight/AuthenticationCheckVC.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationCheckVC.swift
+//  Spotlight
+//
+//  Created by Tanha Kabir on 2016-08-22.
+//  Copyright © 2016 SpotlightTeam. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationCheckVC: UIViewController {
+    override func viewDidLoad() {
+        //
+    }
+    
+    override func didReceiveMemoryWarning() {
+        //
+    }
+
+}
---
+++ Spotlight/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Spotlight
+//
+//  Created by Tanha Kabir on 2016-08-22.
+//  Copyright © 2016 SpotlightTeam. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    override func viewDidLoad() {
+        //
+    }
+    
+    override func didReceiveMemoryWarning() {
+        //
+    }
+
+}
